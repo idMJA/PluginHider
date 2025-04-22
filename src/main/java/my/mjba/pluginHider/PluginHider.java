44d37cff47001a4a4bbe1654666f1befc12b2bea
iaMJ <kiyomi@mjba.my>
2025-04-22 20:24:00 +0700

    cleanup

--- src/main/java/my/mjba/pluginHider/PluginHider.java
+++ src/main/java/my/mjba/pluginHider/PluginHider.java
@@ -1,6 +1,5 @@
 package my.mjba.pluginHider;
 
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,10 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.TabCompleteEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class PluginHider extends JavaPlugin implements Listener {
     private boolean enabled;
